987a3a578e4a72735ebd3490816ce1154df2c5cc
careinicke <35277845+careinicke@users.noreply.github.com>
2018-03-18 19:46:11 -0400

    Add files via upload

---
+++ variablehw.js
@@ -0,0 +1,5 @@
+
+var houseHeadEnglish= yes/ no;
+var grant = $1000;
+var NGOs= ["Help Unlimited", "New Start", "Better Tomorrow", "Home Safe", "New Home"]
+var familyExpense= family: [1, 2, 3, 4,5] Expense:[800,950,1000,1100,1200]
